88ef1efa7355423dba07678491a99cf1b260dc77
saurabh26398 <sourabhjangid263@gmail.com>
2026-02-07 23:34:08 +0530

    removed duplicate line for server name

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
 const io = new Server(server, {
     cors: {
         origin: "*", // Allows any device to connect
